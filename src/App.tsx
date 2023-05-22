463ea5bd77cb38bb5cdfe72a96358c34f426f153
gylman <gylmanbm@gmail.com>
2023-05-22 16:57:44 +0900

    basic structure without suggested tokens

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import Search from "./components/Search";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Search></Search>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import Input from "./Input";
+import TableRow from "./TableRow";
 
 type Props = {};
 
+const TableWrapper = styled.div`
+  padding: 58px 44px 18px 44px;
+  height: 581px;
+  width: 717px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f9f9f9;
+`;
+
+const Bridge = styled.p`
+  text-decoration: underline;
+  margin-top: 18px;
+`;
+
 const Search = (props: Props) => {
-  return <div>Search</div>;
+  return (
+    <TableWrapper>
+      <Input />
+      <TableRow />
+      <Bridge>Bridge tokens to Polygon</Bridge>
+    </TableWrapper>
+  );
 };
 
 export default Search;

eb3218882a6b4653e009a62e202cd06b70683a13
gylman <gylmanbm@gmail.com>
2023-05-24 08:53:46 +0900

    before NO

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 // import TestInput from "./components/Search/InputSearch/TestInput";
 // import Search from "./components/Search/Search";
-import { useState } from "react";
 import ProgressBar from "./components/ProgressBar/ProgressBar";
 import "./index.css";
 

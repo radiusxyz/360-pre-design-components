1f549205161c829b5efeb8faa254585dda2c04a1
gylman <gylmanbm@gmail.com>
2023-05-24 14:17:18 +0900

    pre gap removal

--- src/components/ConfirmationInfo/ConfirmationInfo.tsx
+++ src/components/ConfirmationInfo/ConfirmationInfo.tsx
@@ -1,12 +1,57 @@
-import React from "react";
-import { Wrapper, TransactionDetails } from "./ConfirmationInfoStyles";
+import {
+  Wrapper,
+  TransactionDetails,
+  TokenPair,
+  Token,
+  TokenLogo,
+  TokenDetails,
+  Divider,
+  Column,
+  Header,
+  Data,
+} from "./ConfirmationInfoStyles";
+
+import exchange from "../../assets/images/exchange.png";
 
 type Props = {};
 
 const ConfirmationInfo = (props: Props) => {
   return (
     <Wrapper>
-      <TransactionDetails />
+      <TokenPair>
+        <Token>
+          <TokenLogo />
+          <TokenDetails>0.225 MATIC</TokenDetails>
+        </Token>
+        <img src={exchange} alt="exchange_icon" />
+        <Token>
+          <TokenLogo />
+          <TokenDetails>0.225 DAI</TokenDetails>
+        </Token>
+      </TokenPair>
+      <Divider />
+      <TransactionDetails>
+        <Column>
+          <Header>Time</Header>
+          <Data>60sec</Data>
+        </Column>
+        <Column>
+          <Header>Fee</Header>
+          <Data>No Fee</Data>
+        </Column>
+        <Column>
+          <Header>Slippage</Header>
+          <Data>0.01%</Data>
+        </Column>
+        <Column>
+          <Header>Extra profit</Header>
+          <Data>0.012 MATIC</Data>
+        </Column>
+        <Column>
+          <Header>Your save</Header>
+          <Data>0.058 MATIC</Data>
+        </Column>
+      </TransactionDetails>
     </Wrapper>
   );
 };
--- src/components/ConfirmationInfo/ConfirmationInfoStyles.tsx
+++ src/components/ConfirmationInfo/ConfirmationInfoStyles.tsx
@@ -4,36 +4,71 @@ export const Wrapper = styled.div`
   background-color: #f5f5f5;
   padding: 44px 42px 40px 42px;
   max-height: 296px;
-  max-width: 913px;
   height: 100%;
   width: 100%;
 `;
 
-export const TokenPair = styled.div``;
+export const TokenPair = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 97px;
+  margin-bottom: 38px;
+  overflow: hidden;
+`;
+
+export const Token = styled.div`
+  display: flex;
+  gap: 13px;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TokenLogo = styled.div`
+  max-width: 57px;
+  max-height: 57px;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #cacaca;
+`;
+
+export const TokenDetails = styled.p`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 21px;
+  color: #666666;
+  white-space: nowrap;
+`;
 
 export const Divider = styled.div`
   width: 100%;
   height: 1px;
   background-color: #d6d6d6;
+  margin-bottom: 33px;
 `;
 
 export const TransactionDetails = styled.div`
+  width: 100%;
   display: flex;
-  align-items: center;
+  align-items: start;
+  justify-content: center;
+  gap: 90px;
+  overflow: hidden;
 `;
 
 export const Column = styled.div`
   display: flex;
   flex-direction: column;
   gap: 12px;
-`;
+ff`;
 
 export const Header = styled.p`
   font-weight: 500;
   font-size: 14px;
   line-height: 17px;
-  text-align: center;
   color: #999999;
+  white-space: nowrap;
 `;
 
 export const Data = styled.p`
@@ -41,6 +76,7 @@ export const Data = styled.p`
   font-weight: 500;
   font-size: 18px;
   line-height: 21px;
-  text-align: center;
   color: #333333;
+  width: auto;
+  white-space: nowrap;
 `;

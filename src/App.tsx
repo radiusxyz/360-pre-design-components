e75bbeea4622d376744e3047742c2b86ad4d3c7c
gylman <gylmanbm@gmail.com>
2023-05-24 10:36:20 +0900

    test version

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,16 @@ import "./index.css";
 function App() {
   return (
     <div className="App">
-      <ProgressBar status={3} />
+      <div style={{ display: "flex", flexDirection: "column", gap: 20 }}>
+        <p style={{ textAlign: "center" }}>Start</p>
+        <ProgressBar status={0} />
+        <p style={{ textAlign: "center" }}>InProgress</p>
+        <ProgressBar status={1} />
+        <p style={{ textAlign: "center" }}>Success</p>
+        <ProgressBar status={2} />
+        <p style={{ textAlign: "center" }}>Fail</p>
+        <ProgressBar status={3} />
+      </div>
     </div>
   );
 }
--- src/components/ProgressBar/StyledProgressBar.tsx
+++ src/components/ProgressBar/StyledProgressBar.tsx
@@ -4,5 +4,6 @@ export const StyledProgressBar = styled.div`
   display: flex;
   gap: 5px;
   align-items: center;
-  width: 700px;
+  width: 100%;
+  padding: 10px 100px 10px 100px;
 `;

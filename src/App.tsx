98f1cb34a9033306aa0d61e2b815bd24d16ef2be
gylman <gylmanbm@gmail.com>
2023-05-25 14:43:51 +0900

    implement search tokens component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Search from "./components/Search/Search";
 import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Search />
+    </div>
+  );
 }
 
 export default App;

e4d6faa33c43de4e7e09618601ea25587e15d7bb
gylman <gylmanbm@gmail.com>
2023-05-25 13:55:03 +0900

    implement vertically emerging and hiding div with open/close buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,27 @@
-// import TestInput from "./components/Search/InputSearch/TestInput";
-// import Search from "./components/Search/Search";
-import ConfirmationInfo from "./components/ConfirmationInfo/ConfirmationInfo";
-import ProgressBar from "./components/ProgressBar/ProgressBar";
 import "./index.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <div
-        className="progress"
-        style={{ display: "flex", flexDirection: "column", gap: 20 }}
-      >
-        <p style={{ textAlign: "center" }}>Start</p>
-        <ProgressBar status={0} />
-        <p style={{ textAlign: "center" }}>InProgress</p>
-        <ProgressBar status={1} />
-        <p style={{ textAlign: "center" }}>Success</p>
-        <ProgressBar status={2} />
-        <p style={{ textAlign: "center" }}>Fail</p>
-        <ProgressBar status={3} />
-      </div>
-      <ConfirmationInfo
-        details={{
-          from: { token: "MATIC", amount: 0.225 },
-          to: { token: "DAI", amount: 0.225 },
-          time: 60,
-          fee: "No Fee",
-          slippage: 0.01,
-          extraProfit: 0.012,
-          yourSave: 0.058,
-        }}
-      />
+  const [myStyle, setMyStyleStyle] = useState("pre-final");
 
-      {/* <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          minWidth: "auto",
-        }}
-      >
-        <div style={{ backgroundColor: "red", height: 50, width: "auto" }}>
-          Hoihoihoi
-        </div>
-        <div style={{ backgroundColor: "red", height: 50, width: "auto" }}>
-          Hihi
-        </div>
-        <div style={{ backgroundColor: "red", height: 50, width: "auto" }}>
-          OiOi
-        </div>
-      </div> */}
+  const handleClick = () => {
+    setMyStyleStyle((myStyle) =>
+      myStyle === "pre-final" ? "final" : "pre-final"
+    );
+  };
+
+  return (
+    <div className="App" style={{ position: "relative" }}>
+      <div className="initial">
+        <button onClick={handleClick} className="btn">
+          OPEN
+        </button>
+      </div>
+      <div className={myStyle}>
+        <button onClick={handleClick} className="btn-cls">
+          CLOSE
+        </button>
+      </div>
     </div>
   );
 }

359fcf703faec1f9b2f360dad89c2b4ab74ef6f5
gylman <gylmanbm@gmail.com>
2023-06-06 10:59:51 +0900

    training

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="mother">
+        <div className="c-1">This is first div</div>
+        <div className="c-2">This is second div</div>
+        <div className="c-3">This is third div</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
